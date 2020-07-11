2244816955356e292d376f1190d97e7eb05bc8e5
David Mogbeyi <mogbeyidavid@gmail.com>
2020-07-11 03:49:12 +0100

    Added some method

--- second-project/functions.js
+++ second-project/functions.js
@@ -7,4 +7,12 @@ const getGithubReposForUser = async (username) => {
     return result.length;
 };
 
+const getJsonResponse = async (link) => {
+    //'https://jsonplaceholder.typicode.com/todos/1'
+    const response = await fetch(link);
+    const result = await response.json();
+    const {id} = result;
+    return id
+};
+
 module.exports = {getGithubReposForUser};
