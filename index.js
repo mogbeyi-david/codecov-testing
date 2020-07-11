755e0d86f4eccdf6321704521e4d1c6f37cfa63e
David Mogbeyi <mogbeyidavid@gmail.com>
2020-07-11 02:22:08 +0100

    work

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const add = (firstNumber, secondNumber) => {
-  return firstNumber + secondNumber;
+    return firstNumber + secondNumber;
 };
 
 const getDifference = (firstNumber, secondNumber) => {
-  return Math.abs(firstNumber - secondNumber);
+    return Math.abs(firstNumber - secondNumber);
 };
 
 const multiply = (firstNumber, secondNumber) => {
-  return firstNumber * secondNumber;
+    return firstNumber * secondNumber;
+};
+
+const getQuotient = (firstNumber, secondNumber) => {
+    return Math.floor(firstNumber / secondNumber);
 };
 
 module.exports = {
-  add,
-  getDifference,
-  multiply,
+    add,
+    getDifference,
+    multiply,
 };
