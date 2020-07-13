3b40acb0599deb2300358a9fa1c92517c3c361ba
David Mogbeyi <mogbeyidavid@gmail.com>
2020-07-13 01:26:59 +0100

    Added more tests

--- second-project/index.test.js
+++ second-project/index.test.js
@@ -7,7 +7,7 @@ describe("Get Github Repository Information for User", () => {
 });
 
 describe("Get Some Random JSON Response", () => {
-    // it('should an id of 1', async () => {
-    //     expect(await getJsonResponse("https://jsonplaceholder.typicode.com/todos/1")).toEqual(1);
-    // });
+    it('should an id of 1', async () => {
+        expect(await getJsonResponse("https://jsonplaceholder.typicode.com/todos/1")).toEqual(1);
+    });
 });
